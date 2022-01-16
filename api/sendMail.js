19d169512c03dc7122c043cad7297998bf707c0a
Tatianka-hk <t0681689150@gmail.com>
2022-01-16 21:48:50 +0200

    HELLO WORLD!

--- api/sendMail.js
+++ api/sendMail.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 const history = new Map();
 
 const transport = createTransport({
-  host: process.env.HOST,
-  port: process.env.POST,
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
   secure: false,
   auth: {
     user: process.env.EMAIL_ADRESS,
